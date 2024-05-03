7cdd69ef44b9f363047c7225a50ed45af9f3584b
Nicolas Richart <nicolas.richart@epfl.ch>
2024-05-03 15:09:57 +0200

    custom changes

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
    '((TeX-command-extra-options . "-shell-escape")
@@ -10,7 +12,8 @@
      (flycheck-checker . c/c++-clang-tidy)
      (eval set-background-color "#000015")
      (projectile-enable-caching . t)
-     (projectile-project-name . "Akantu[master]"))))
+     (projectile-project-name . "Akantu[master]")))
+ '(tramp-verbose 6))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
